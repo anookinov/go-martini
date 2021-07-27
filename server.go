f0a67fa6467e6ff42b499d5a2012f5c225d12751
anookinov <anookinov@gmail.com>
2021-07-28 00:39:48 +0700

    added post savefile endpoint with multipart/form-data form for file upload

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -29,6 +31,38 @@ func save(c echo.Context) error {
 	return c.String(http.StatusOK, "name:" + name + ", email:" + email)
 }
 
+// e.POST("/savefile", savefile)
+func savefile(c echo.Context) error {
+	// Get name
+	name := c.FormValue("name")
+	//Get avatar
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return err
+	}
+
+	// Source
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, "<b>Thank you!" + name + "</b>")
+}
+
 func main() {
 	e := echo.New()
 	// e.POST("/users", saveUser)
@@ -37,5 +71,6 @@ func main() {
 	// e.DELETE("/users/:id", deleteUser)
 	e.GET("/show", show)
 	e.POST("/save", save)
+	e.POST("/savefile", savefile)
 	e.Logger.Fatal(e.Start(":1323"))
 }
