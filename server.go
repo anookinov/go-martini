6dabba120c662a2f108f1f3a01721e82d92c6c38
anookinov <anookinov@gmail.com>
2021-07-28 00:14:19 +0700

    added get show with query parameters

--- server.go
+++ server.go
@@ -13,8 +13,17 @@ func getUser(c echo.Context) error {
 	return c.String(http.StatusOK, id)
 }
 
+// e.GET("/show", show)
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
 	e.Logger.Fatal(e.Start(":1323"))
 }
